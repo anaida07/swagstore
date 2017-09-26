57056be4fa116e2251eb8a95ecf09afa7a5deb36
Aneeta Sharma <get.aneeta@gmail.com>
2017-09-26 08:36:44 +0545

    Init project.

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <header>
+      <b-container class='products_wrapper'>
+        <b-row>
+          <b-col cols='4'><img src='./assets/images/cf-logo-header.png' /></b-col>
+          <b-col>
+            <ul>
+              <li><a href="#"><i class="fa fa-user-o"></i></a></li>
+              <li><a href="#"><i class="fa fa-heart-o"></i></a></li>
+              <li><a href="#"><i class="fa fa-shopping-basket"></i></a></li>
+              <li><a href="#"><i class="fa fa-th-large"></i></a></li>
+            </ul>
+          </b-col>
+        </b-row>
+      </b-container>
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
---
+++ client/src/components/Products.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class='product_wrapper'>
+  	<b-container>
+  		<b-row>
+  			<b-col cols='4'>
+  				<a href="/products/1">
+  					<div class="block">
+  						<img src='../assets/images/bottle.png' />
+  					</div>
+  				</a>
+  			</b-col>
+  			<b-col cols='4'>
+  				<a href="#">
+  					<div class="block">
+  						<img src='../assets/images/cup.jpg' />
+  					</div>
+  				</a>
+  			</b-col>
+  			<b-col cols='4'>
+  				<a href="#">
+  					<div class="block">
+  						<img src='../assets/images/lion.png' />
+  					</div>
+  				</a>
+  			</b-col>
+  			<div class="w-100"></div>
+  			<b-col cols='4'>
+  				<a href="#">
+  					<div class="block">
+  						<img src='../assets/images/lion.png' />
+  					</div>
+  				</a>
+  			</b-col>
+  			<b-col cols='4'>
+  				<a href="#">
+  					<div class="block">
+  						<img src='../assets/images/lion.png' />
+  					</div>
+  				</a>
+  			</b-col>
+  			<b-col cols='4'>
+  				<a href="#">
+  					<div class="block">
+  						<img src='../assets/images/lion.png' />
+  					</div>
+  				</a>
+  			</b-col>
+  		</b-row>
+  	</b-container>
+  </div>
+</template>
